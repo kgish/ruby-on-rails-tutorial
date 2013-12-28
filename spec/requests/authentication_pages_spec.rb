e4fd9b2f1de4ff9a439d7829ed9c1a3ea482e322
Kiffin Gish <kiffin.gish@planet.nl>
2013-12-28 20:57:03 +0100

    Completed Chapter 9.6 exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,6 +49,21 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "when attempting to visit the home page" do
+        before { visit root_path }
+
+        describe "only certain links should be visible" do
+
+          it { should have_link('Sign in',      href: signin_path) }
+          it { should have_link('Help',         href: help_path) }
+          it { should_not have_link('Users',    href: users_path) }
+          it { should_not have_link('Profile',  href: user_path(user)) }
+          it { should_not have_link('Settings', href: edit_user_path(user)) }
+          it { should_not have_link('Sign out', href: signout_path) }
+
+        end
+      end
+
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -61,6 +76,20 @@ describe "Authentication" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          define "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
@@ -109,6 +138,23 @@ describe "Authentication" do
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
+    describe "as signed-in user" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before { sign_in user, no_capybara: true }
+
+      describe "accessing Users#create action" do
+        before { post users_path, :user => {:name => "user", :email => "user@example.com", 
+                                        :password => "password", :password_confirmation => "password"} }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+      describe "accessing Users#new action" do
+        before { get new_user_path }
         specify { expect(response).to redirect_to(root_url) }
       end
     end
