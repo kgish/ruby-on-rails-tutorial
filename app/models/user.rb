dbd99a4e3923a6e62a8290c133c4ba65baf3f60d
Kiffin Gish <kiffin.gish@planet.nl>
2014-01-02 12:16:55 +0100

    Completed chapter 10.3.3 A proto-feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,18 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
 
-def User.new_remember_token
-  SecureRandom.urlsafe_base64
-end
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
-def User.encrypt(token)
-  Digest::SHA1.hexdigest(token.to_s)
-end
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
 
   private
 
